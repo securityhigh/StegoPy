97b4eef1351f7185b5ec683e48eeb001eb72b425
eBind Group <45529919+eBind@users.noreply.github.com>
2020-06-01 10:05:51 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,5 @@ fi
 
 # Installation
 sudo pip install -r requirements.txt
-sudo cp stegopy.py /usr/local/bin/stegopy.py
-sudo chmod +x /usr/local/bin/stegopy.py
-sudo mv /usr/local/bin/stegopy.py /usr/local/bin/stegopy
+sudo cp stegopy.py /usr/local/bin/stegopy
+sudo chmod +x /usr/local/bin/stegopy
